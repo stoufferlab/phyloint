b490ce8e6a646967a73a969313f796765308f409
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2015-06-17 09:34:14 +1200

    removing all traces of simulated annealing code from namespace

--- R/sa.start.R
+++ R/sa.start.R
@@ -4,7 +4,7 @@
 #' @param prob The target initial probability of accepting a move; defaults to 0.50
 #' @param reps The number of reps to estimate across; defaults to 100
 #' @param start.type Whether the target probability refers to accepting the min, mean, or max change; defaults to mean
-#' @export
+#' 
 `sa.start` <- function(adj, phy, prob = 0.50, reps = 100, start.type = 'mean')
 {
     gaps <- sum.phylo.gaps(adj,phy)
