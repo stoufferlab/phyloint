d9581478651526fdb59dfaba8b0d62eb7842f51a
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2015-07-17 10:32:20 +1200

    adding sample data and corresponding documentation

---
+++ R/eklof.R
@@ -0,0 +1,10 @@
+#' Sample food-web dataset with which to study phylogenetic intervality
+#'
+#' The object 'eklof' has two items: a 'network' object and a 'tree' object.
+#' The 'network' object is a binary SxS matrix where a value of 1 indicates
+#' that the row species consumes the column species. The 'tree' object is a
+#' phylogenetic tree of class 'phylo' (see 'ape::read.tree'). The data are a
+#' synthetic dataset created to demonstrate how to use the 'phyloint' package
+#' and therefore do not have an associated reference.
+#'
+"eklof"
