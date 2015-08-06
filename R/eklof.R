1129cc3972ead1fe0b0c0bcce744219b8d659875
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2015-08-07 09:17:22 +1200

    adding sample traits to the sample dataset

--- R/eklof.R
+++ R/eklof.R
@@ -1,10 +1,13 @@
 #' Sample food-web dataset with which to study phylogenetic intervality
 #'
-#' The object 'eklof' has two items: a 'network' object and a 'tree' object.
-#' The 'network' object is a binary SxS matrix where a value of 1 indicates
-#' that the row species consumes the column species. The 'tree' object is a
-#' phylogenetic tree of class 'phylo' (see 'ape::read.tree'). The data are a
-#' synthetic dataset created to demonstrate how to use the 'phyloint' package
-#' and therefore do not have an associated reference.
+#' The object 'eklof' has three items: a 'network' object, a 'tree' object, 
+#' and a 'traits' object. The 'network' object is a binary SxS matrix where
+#' a value of 1 indicates that the row species consumes the column species.
+#' The 'tree' object is a phylogenetic tree of class 'phylo' (see 
+#' 'ape::read.tree'). The 'traits' object is a data frame with species names
+#' as row names and columns for different species traits (which can be both
+#' continuous or categorical). These data constitute a synthetic dataset 
+#' created to demonstrate how to use the 'phyloint' package and hence do
+#' not have an associated reference.
 #'
 "eklof"
